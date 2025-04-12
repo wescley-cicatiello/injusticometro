eb5d68d1bd9afb0d55afe50a1b3d722544b5bc82
Wescley <wescleycicatiello@gmail.com>
2025-04-12 20:03:33 -0300

    acerto conexão com api

--- public/summoner_info.php
+++ public/summoner_info.php
@@ -2,12 +2,13 @@
 require '../riotkeymanager/riot_key_manager.php';
 
 // VALIDA POST
-if ($_SERVER['REQUEST_METHOD'] !== 'POST' || empty($_POST['region']) || empty($_POST['summoner'])) {
+if ($_SERVER['REQUEST_METHOD'] !== 'POST' || empty($_POST['region']) || empty($_POST['summoner']) || empty($_POST['tagline'])) {
     die("❌ Região e nome do invocador são obrigatórios.");
 }
 
 $region = $_POST['region'];
 $summonerName = trim($_POST['summoner']);
+$tagline = $_POST['tagline'];
 
 // CONFIGURAÇÕES DO BANCO
 $dbHost = 'localhost';
@@ -33,36 +34,67 @@ if (!$apiKey) {
 }
 
 // REQUISIÇÃO PARA A API DA RIOT
-$url = "https://$region.api.riotgames.com/lol/summoner/v4/summoners/by-name/" . urlencode($summonerName);
 
-$ch = curl_init();
-curl_setopt_array($ch, [
-    CURLOPT_URL => $url,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HTTPHEADER => [
-        "X-Riot-Token: $apiKey"
-    ]
-]);
+
+$url = "https://$region.api.riotgames.com/riot/account/v1/accounts/by-riot-id/$summonerName/$tagline?api_key=$apiKey";
+
+
+$ch = curl_init($url);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt_array($ch, [
+//     CURLOPT_RETURNTRANSFER => true
+// ]);
+
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+$err = curl_error($ch);
 
 $response = curl_exec($ch);
-$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+$map = json_decode($response, true);
+
 curl_close($ch);
 
+var_dump("Código nº: $httpCode");
+if ($err != null && strlen($err) > 0) var_dump("Erro: $err");
+
+var_dump($map);
+var_dump($map["puuid"]);
+var_dump($map["gameName"]);
+
+https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/ZbqamuYdo-QsQJWhtpoAVmiQKkCpVrwDY7YJqA_d6sh1pOB52I_g8oSCWex6F79IhFsZcBIOY5YP6A?api_key=RGAPI-67cfb242-4bcb-4b07-a997-e370a6056f0a
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // TRATA E MOSTRA O RESULTADO
-if ($httpCode === 200) {
-    $data = json_decode($response, true);
-
-    echo "<h2>🔍 Resultado para: " . htmlspecialchars($summonerName) . "</h2>";
-    echo "<ul>";
-    echo "<li><strong>ID:</strong> {$data['id']}</li>";
-    echo "<li><strong>PUUID:</strong> {$data['puuid']}</li>";
-    echo "<li><strong>Nível:</strong> {$data['summonerLevel']}</li>";
-    echo "</ul>";
-} else {
-    echo "❌ Erro ao buscar dados do invocador. Código HTTP: $httpCode<br>";
-    echo "Resposta: <pre>$response</pre>";
-}
-
-echo "<pre>API Key (descriptografada): $apiKey</pre>";
+// if ($httpCode === 200) {
+//     $data = json_decode($response, true);
+//         var_dump($response);
+//     echo "<h2>🔍 Resultado para: " . htmlspecialchars($summonerName) . "</h2>";
+//     echo "<ul>";
+//     echo "<li><strong>ID:</strong> {$data['id']}</li>";
+//     echo "<li><strong>PUUID:</strong> {$data['puuid']}</li>";
+//     echo "<li><strong>Nível:</strong> {$data['summonerLevel']}</li>";
+//     echo "</ul>";
+// } else {
+//     echo "❌ Erro ao buscar dados do invocador. Código HTTP: $httpCode<br>";
+//     echo "Resposta: <pre>$response</pre>";
+// }
 
 ?>
